e71143cf80cff5ea07632ad1979449a139f06b42
Z-Link-C <zachery.croft@gmail.com>
2026-05-07 06:16:13 -0400

    Ready for grading ..finally

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self,title,page_count):
+        self.title=title
+        self.page_count=int(page_count)
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+    def get_count(self):
+        return self._page_count
+    def set_count(self,count):
+        if(type(count) is int and 0<=count):
+            self._page_count=count
+        else:
+            print("page_count must be an integer") 
+    page_count=property(get_count,set_count)       
